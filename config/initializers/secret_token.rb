ff02622757cbddc31aeeb2fc40bba835cf39b340
Proman21 <snoopyk921@gmail.com>
2012-10-04 20:04:27 +1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TopicWall::Application.config.secret_token = '4daf754db25b205794f35a3b57d8f9836ab4e89aaac2b79fe6676ae3e95d7173ee7ae6400b74921b645379e3c09b0ce4232c644001a4437c872f3ed3a77c9242'
