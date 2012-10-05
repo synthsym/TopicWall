942134df88d50544913e963cddbb62822e33d558
Proman21 <snoopyk921@gmail.com>
2012-10-05 11:22:17 +1000

    cleaned up some code
    Signed-off-by: Proman21 <snoopyk921@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable,
-  # :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :token_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
